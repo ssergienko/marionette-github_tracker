d26a5e77da57e524fd97a17e940a228104ac9566
ssergienko <sergey.s.sergienko@gmail.com>
2015-06-30 21:30:00 +0300

    Forgotten test added

---
+++ tests/specs/ContributersController_test.js
@@ -0,0 +1,8 @@
+(function() {
+  describe("Contributers.Controller", function() {
+    return it("test method exists and executes", function() {
+      return expect(GithubApp.GithubApp.Contributers.Controller.test()).toEqual('test');
+    });
+  });
+
+}).call(this);
